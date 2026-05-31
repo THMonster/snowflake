3950a57c63fbf8429aa46361df1bf259e6e8331e
THMonster <5125031+THMonster@users.noreply.github.com>
2026-05-31 10:21:11 +0800

    add oranc

--- flake.nix
+++ flake.nix
@@ -9,39 +9,25 @@
 
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-		nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
-		nur.url = "github:nix-community/NUR";
-		utils.url = "github:numtide/flake-utils";
+		# nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
+		# nur.url = "github:nix-community/NUR";
+		# utils.url = "github:numtide/flake-utils";
 		# naersk.url = "github:nix-community/naersk";
 	};
 
 
-	outputs = { self, nixpkgs, nixpkgs-stable, nur, ... } @ inputs: 
+	outputs = { self, nixpkgs, ... } @ inputs: 
+	let
+		lib = nixpkgs.lib;
+		forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+	in {
+		packages = forAllSystems (system:
 		let
-			# nixpkgs_system = system:
-			#   import nixpkgs {
-			#     inherit system;
-			#     config.allowUnfree = true;
-			#   };
+			pkgs = import nixpkgs { inherit system; };
 		in {
-		} // inputs.utils.lib.eachDefaultSystem (system: 
-		let
-			pkgs = nixpkgs.legacyPackages.${system};
-			# linux_6_1_r5c = pkgs.callPackage ./pkgs/linux-r5c { };
-			# linux_6_8_r5c = pkgs.callPackage ./pkgs/linux-r5c-6-8 { };
-			# linux_6_6_r5c = pkgs.callPackage ./pkgs/linux-r5c-6-6 { };
-		in {
-			legacyPackages = {
-			};
-			packages = {
-				# inherit linux_6_1_r5c;
-				# inherit linux_6_8_r5c;
-				# inherit linux_6_6_r5c;
-				# linuxPackages_r5c_6_8 = pkgs.linuxPackagesFor linux_6_8_r5c;
-				# linuxPackages_r5c_6_6 = pkgs.linuxPackagesFor linux_6_6_r5c;
-				linuxPackages_r5c = import ./pkgs/linux-r5c { inherit pkgs; };
-				tpconenatd = pkgs.callPackage ./pkgs/tpconenatd { };
-				archlinux-repo = pkgs.callPackage ./pkgs/archlinux-repo { };
-			};
+			linuxPackages_r5c = import ./pkgs/linux-r5c { inherit pkgs; };
+			tpconenatd = pkgs.callPackage ./pkgs/tpconenatd { };
+			archlinux-repo = pkgs.callPackage ./pkgs/archlinux-repo { };
 		});
+	};
 }
